5db5e564e3418fd829d54ca3e67fbb2630e6fe69
evgeniy <djon.1994@yandex.ru>
2022-03-15 08:49:46 +0300

    Build finih

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -2,10 +2,30 @@
 //
 
 #include <iostream>
+//вызываем функцию std::cout
+void print()
+{
+    std::cout <<"Hello skillbox\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x=100;
+    3+7;
+    int y=x+100;
+
+    int b=0;
+    b=b+2;
+
+    int test;
+    test=100500;
+
+    int test2=10501;
+
+    int mult =x*y;
+    int random=1002;
+
+    std::cout<<random;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
